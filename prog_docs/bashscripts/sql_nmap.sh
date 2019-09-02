07927af4da37e2cf064a9b38476f754da07fb1ad
csparks <csparksnj@gmail.com>
2019-09-02 01:17:48 +0000

    cleaned up prog_docs

--- prog_docs/bashscripts/sql_nmap.sh
+++ prog_docs/bashscripts/sql_nmap.sh
@@ -1,6 +1,4 @@
+#!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "Please pass in a ip"
-else
-  nmap -sV -Pn -vv --script=mysql-audit,mysql-databases,mysql-dump-hashes,mysql-empty-password,mysql-enum,mysql-info,mysql-query,mysql-users,mysql-variables,mysql-vuln-cve2012-2122 $1 -p 3306
-fi
+nmap -sV -Pn -vv --script=mysql-audit,mysql-databases,mysql-dump-hashes,mysql-empty-password,mysql-enum,mysql-info,mysql-query,mysql-users,mysql-variables,mysql-vuln-cve2012-2122 $ip -p 3306
+
