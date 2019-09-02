f7bd793dedcb5a19a73baa1a03bc0efcd997ff8e
csparks <csparksnj@gmail.com>
2019-09-02 00:14:47 +0000

    transfered all to database; added color; added helperfile

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
 import sqlite3
 import prog_docs.text_content as txt
+from colorama import init, Fore, Back, Style
+init(autoreset=True)
 
 wa = txt.web_app_commands
 sql = txt.sql_commands
@@ -16,7 +18,12 @@ ssl = txt.ssl
 smtp = txt.smtp_commands
 rpc = txt.rpc
 
-all_commands = {"sql":sql, "pks":pks, "lists":lists, "nmap":nmap, "dns":dns, "smb":smb, "nfs":nfs, "enum":enum, "ssh":ssh, "ftp":ftp, "ssl":ssl, "smtp":smtp, "rpc":rpc}
+all_commands = {"sql":sql, "pks":pks, "lists":lists, "nmap":nmap, "dns":dns, "smb":smb, "nfs":nfs, "enum":enum, "ssh":ssh, "ftp":ftp, "ssl":ssl, "smtp":smtp, "rpc":rpc,"web_apps":wa}
+def cy_on_ma(string):
+    return Style.BRIGHT + Fore.CYAN + string + Fore.RESET + Style.RESET_ALL
+
+def green(string):
+    return Style.BRIGHT + Fore.GREEN + string + Fore.RESET + Style.RESET_ALL
 
 def extract(l, m):
     for l1 in l:
@@ -25,11 +32,12 @@ def extract(l, m):
             if "COMMANDS" in topic:
                 pass
             else:
-                command = l1[1].replace("\x1b[33m", "")
+                stripped =  l1[1].replace("\x1b[33m", "")
+                command = cy_on_ma(stripped)
                 description = l1[2]
-                insert_into_db(topic, command, description, m)
-        except:
-            print "Couldnt enter: " + str(command)
+                insert_into_db(topic, command, green(description), m)
+        except Exception, e:
+            print e
 
 def insert_into_db(topic, command, description, mod_name):
     conn = sqlite3.connect("offsec.db")
