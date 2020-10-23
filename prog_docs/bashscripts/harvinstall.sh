42c9ad4f31a3ebe8aaaca8f3d5d6100b362bce42
chrissparks <csparksnj@gmail.com>
2020-10-23 04:22:46 +0000

    fixed install script for harvester install

--- prog_docs/bashscripts/harvinstall.sh
+++ prog_docs/bashscripts/harvinstall.sh
@@ -4,9 +4,10 @@ cd theHarvester
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt install python3.7
-sudo apt-get install python3.7-pip
+sudo apt install python3-pip
 pip3 install -r requirements/base.txt
 pip3 install aiohttp
-echo "alias python=python3.7" >> ~/.bashrc
+
+echo "alias python=python3" >> ~/.bashrc
 source ~/.bashrc 
-echo "HARVESTER INSTALLED. RUN theHarvester.py -h"
+echo "HARVESTER INSTALLED. RUN python3 theHarvester.py -h"
