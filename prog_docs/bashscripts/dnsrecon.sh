cf1bce26bad83aec46bf297c7400d9cab413288e
chrissparks <csparksnj@gmail.com>
2020-10-26 23:18:46 +0000

    searchsploit and dnsrecon scripts

---
+++ prog_docs/bashscripts/dnsrecon.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/darkoperator/dnsrecon.git
+cd dnsrecon
+pip install -r requirements.txt
+python3 dnsrecon.py -h
---
+++ prog_docs/bashscripts/searchsploit.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
+sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
+searchsploit
